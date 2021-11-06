9c5494c0b8ff3cf968a1e43ce36a6cff73bbd5b1
Juho Salli <juho.salli@gmail.com>
2021-11-06 10:51:04 +0200

    Fetches visitors and pageviews
    And saves them into a file

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 # Netlify Analytics Script
 
-Get data from the undocumented Netlify Analytics API.
+Get data from the undocumented Netlify Analytics API and save it as a csv-file.
+
+Currently fetches pageviews and visitors for the previous 30 days.
 
 ## Usage
 
-1. Figure out your `page_id` and your `bearer token`.
-2. Copy `.env-example`, rename it to `.env`
+1. Figure out your `page_id` and your `bearer token`. (network tab is your friend here)
+2. Copy `.env-example` and rename it to `.env`
 3. Insert the `page_id` and `bearer token` to correct places in the `.env`-file.
+4. `npm install`
+5. `node script.js`
+6. Check `data`-directory for the data
 
 ## Disclaimer
 
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// const dotenv = require("dotenv");
 import dotenv from "dotenv";
 import fetch from "node-fetch";
+import fs from "fs";
 
 dotenv.config();
 
@@ -9,11 +9,8 @@ const PAGE_ID = process.env.NTL_PAGE_ID;
 
 const baseUrl = `https://analytics.services.netlify.com/v2/${PAGE_ID}`;
 
-const getPageViews = async () => {
-  let from = "1633381200000";
-  let to = "1636006082559";
-
-  const pageViewsUrl = `${baseUrl}/visitors?from=${from}&to=${to}&timezone=%2B0200&resolution=day`;
+const getData = async (from, to, type, resolution = "day") => {
+  const pageViewsUrl = `${baseUrl}/${type}?from=${from}&to=${to}&timezone=%2B0200&resolution=${resolution}`;
 
   let response = await fetch(pageViewsUrl, {
     method: "GET",
@@ -23,7 +20,43 @@ const getPageViews = async () => {
   });
 
   const data = await response.json();
-  console.log(data);
+  return data.data;
 };
 
-getPageViews();
+const processData = (data) => {
+  // change the epoch timestamp to some more readable
+  let new_data = data.map((d) => {
+    let [epoch, count] = d;
+    const date = new Date(epoch);
+    return [date.toISOString(), count];
+  });
+
+  return new_data;
+};
+
+const writeFile = (data, type) => {
+  let csv = "date,count\r\n";
+  for (let row of data) {
+    csv += row.join(",") + "\r\n";
+  }
+
+  fs.writeFileSync(`data/${type}-data.csv`, csv);
+};
+
+// from 30 days ago to today
+// because it looks you can't query longer time periods
+// from the API (atleast pageviews)
+const today = new Date();
+const to = today.getTime();
+const from = today.setTime(today.getDate() - 30);
+
+const types = ["visitors", "pageviews"];
+
+for (let type of types) {
+  console.log(`Working on ${type}`);
+  const visitors = await getData(from, to, type);
+
+  const processed_data = processData(visitors);
+
+  writeFile(processed_data, type);
+}
